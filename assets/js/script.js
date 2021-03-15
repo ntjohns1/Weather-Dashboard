9143a4ec5e02ba31ee4109d7d603c4113012dd4f
ntjohns1 <nelsontjohns@gmail.com>
2021-03-15 04:07:16 -0400

    js functionality for current weather complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,12 @@ WHEN I search for a city
 // get form element
 // get search button click
 
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-use the city search to apply the lat/lon element to the call
+
 // append a div for the city
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+
 // apply text content to the top weather data card
-WHEN I view the UV index
-THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// apply class with conditionals like we did in the last assignment
+
+
 WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 WHEN I click on a city in the search history
@@ -23,7 +20,8 @@ THEN I am again presented with current and future conditions for that city
 var userFormEl = document.querySelector('#user-form');
 var searchInputEl = document.querySelector('#search-field');
 var searchButton = document.querySelector('#submit');
-var cityDate = document.querySelector('#city-date');
+var cityDate = document.querySelector('#city-name');
+var date = document.getElementById('date');
 var icon = document.querySelector('#icon');
 var temp = document.querySelector('#temp');
 var humidity = document.querySelector('#humidity');
@@ -37,7 +35,7 @@ var day5 = document.querySelector('#day5');
 
 // function to clear out form see review activity solution
 
-
+// THEN I am presented with current and future conditions for that city and that city is added to the search history use the city search to apply the lat/lon element to the call
 // calls geocoding API to use lat and lon in next function
 // set up form element text guide that says enter city, state code, and country code
 var getCitySearch = function () {
@@ -48,18 +46,22 @@ var getCitySearch = function () {
   fetch(apiUrl)
     .then(function (response) {
       console.log(response);
-      // figure out how to move .then in line 52 var lat & var lon are up one scope, we can continue it all in one function if we want
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      console.log(data);
+      var cityDate = document.querySelector('#city-name');
+      var currentName = data[0].name;
+      var currentState = data[0].state;
+      var country = data[0].country;
+      cityDate.textContent = currentName + ', ' + currentState + ', ' + country;
+      // when dealing with multiple search results, append boostrap 'select' list to search field
       // for (var i = 0; i < data.length; i++) {
       // console.log(data[i].lat);
       // console.log(data[i].lon);
-      // when dealing with multiple search results, append boostrap 'select' list to search field
       var lat = data[0].lat;
       var lon = data[0].lon;
-      var weathURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=3b41e908f8123a87745091fffda5bb2b';
+      var weathURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=3b41e908f8123a87745091fffda5bb2b';
       console.log(weathURL);
       // }
       fetch(weathURL)
@@ -67,8 +69,23 @@ var getCitySearch = function () {
           return response.json();
         })
         .then(function (data) {
-          console.log(data)
-          // now we can append the data to the page elements here
+          console.log(data);
+
+          /*WHEN I view current weather conditions for that city
+          THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index*/
+          var timeStamp = data.current.dt;
+          console.log(data.current.dt);
+          var today = moment.unix(timeStamp).format("dddd, MMMM Do YYYY");
+          console.log(today);
+          var iconCode = data.current.weather[0].icon;
+          icon.src = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+          temp.textContent = 'Temperature: ' + data.current.temp + ' degrees F';
+          humidity.textContent = 'Humidity: ' + data.current.humidity + '%';
+          windSpeed.textContent = 'Windspeed: ' + data.current.wind_speed + ' mph';
+          uvIndex.textContent = 'UV Index: ' + data.current.uvi;
+          date.textContent = ' - ' + today;
+          // WHEN I view the UV index, THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe.
+          // apply class with conditionals like we did in the last assignment
         });
     });
 }
@@ -83,13 +100,6 @@ var getCitySearch = function () {
 alert('Unable to connect to Open Weather');
 });
 };
-// Function to grab lat and lon variables (lines 58-59) from getCitySearch and plug it into our data search
-// Question: I struggle with scope a bit, where do I need to put this function? I can call get city search in a function
-// we are going to do the same thing with the next fetch = loop through response and grab the desired data[i].item
-// Eventually we will have to deal with mutiple search results, it will involve looping through the city results and appending a selector to the formEl.
-// parse data and print it to elements on page, Bob===Uncle.
-
-
 /*var getFeaturedRepos = function (language) {
 var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
 
