3fea6b048626c61e90aae6f58953c3810f76cfeb
ntjohns1 <nelsontjohns@gmail.com>
2021-03-15 23:12:28 -0400

    added table to html and began js functionality for 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,6 @@
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
+
+// xGIVEN a weather dashboard with form inputs WHEN I search for a city
 // OpenWeather Key = 3b41e908f8123a87745091fffda5bb2b
-// get form element
-// get search button click
-
-
-// append a div for the city
-
-// apply text content to the top weather data card
-
-
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-// use local storage to store the data pull function, we'll need to create an object with the info
-*/
 var userFormEl = document.querySelector('#user-form');
 var searchInputEl = document.querySelector('#search-field');
 var searchButton = document.querySelector('#submit');
@@ -84,15 +68,26 @@ var getCitySearch = function () {
           windSpeed.textContent = 'Windspeed: ' + data.current.wind_speed + ' mph';
           uvIndex.textContent = 'UV Index: ' + data.current.uvi;
           date.textContent = ' - ' + today;
+
+          var tempEls = document.querySelectorAll('.temp');
+          console.log(tempEls);
+          for (let i = 0; i < tempEls.length; i++) {
+            const tempEl = tempEls[i];
+            tempEl.innerText = 'whatever'
+          }
+
           // WHEN I view the UV index, THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe.
           // apply class with conditionals like we did in the last assignment
+          // WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
         });
     });
-}
+};
+// WHEN I click on a city in the search history THEN I am again presented with current and future conditions for that city
+// use local storage to store the data pull function, we'll need to create an object with the info
 
 // Durham, NC, US
-
-/* } else {
+/*
+} else {
   alert('Error: ' + response.statusText);
 }
 })
@@ -100,55 +95,7 @@ var getCitySearch = function () {
 alert('Unable to connect to Open Weather');
 });
 };
-/*var getFeaturedRepos = function (language) {
-var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-fetch(apiUrl).then(function (response) {
-if (response.ok) {
-response.json().then(function (data) {
-  displayRepos(data.items, language);
-});
-} else {
-alert('Error: ' + response.statusText);
-}
-});
-};
-
-var displayRepos = function (repos, searchTerm) {
-if (repos.length === 0) {
-repoContainerEl.textContent = 'No repositories found.';
-return;
-}
-
-repoSearchTerm.textContent = searchTerm;
-
-for (var i = 0; i < repos.length; i++) {
-var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-var repoEl = document.createElement('a');
-repoEl.classList = 'list-item flex-row justify-space-between align-center';
-repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-var titleEl = document.createElement('span');
-titleEl.textContent = repoName;
-
-repoEl.appendChild(titleEl);
-
-var statusEl = document.createElement('span');
-statusEl.classList = 'flex-row align-center';
-
-if (repos[i].open_issues_count > 0) {
-statusEl.innerHTML =
-  "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-} else {
-statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-}
-
-repoEl.appendChild(statusEl);
-
-repoContainerEl.appendChild(repoEl);
-}
-};*/
+*/
 
 
 
