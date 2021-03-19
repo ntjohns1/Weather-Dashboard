d50c69ad22c3015fc045a4213c60fd8c86602757
ntjohns1 <nelsontjohns@gmail.com>
2021-03-18 20:16:01 -0400

    functionality to click buttons works as expected but still not rendering local storage on refresh

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,20 +15,6 @@ var buttonDiv = document.querySelector('#search-history');
 var buttonEl = document.querySelectorAll(".buttons")
 var buttonArray = new Array();
 
-buttonDiv.addEventListener("click", function (e) {
-  e.preventDefault()
-  for (let i = 0; i < buttonEl.length; i++) {
-    var saveClickSearch = e.target.innerHTML;
-    getCitySearch(saveClickSearch)  
-  };
-});
-
-// function to clear out form see review activity solution
-
-// THEN I am presented with current and future conditions for that city and that city is added to the search history 
-// use the city search to apply the lat/lon element to the call
-// calls geocoding API to use lat and lon in next function
-// set up form element text guide that says enter city, state code, and country code
 function getSaves() {
   var searchSaves = JSON.parse(localStorage.getItem("savedSearch"));
   console.log(searchSaves);
@@ -40,18 +26,66 @@ function getSaves() {
       buttonEl.classList.add('buttons');
       buttonEl.setAttribute('type', 'button');
       if (buttonEl.innerHTML === 'undefined' || '[object Object]' || '') {
-      buttonEl.classList.add('hidden');
+        buttonEl.classList.add('hidden');
       }
       buttonDiv.append(buttonEl);
-      buttonArray.push(searchSaves[i]);
     }
   } else {
     return;
   }
 }
+getSaves();
 
-function getCitySearch () {
+
+var formSubmitHandler = function (e) {
+  e.preventDefault();
+
   var searchInput = searchInputEl.value.trim();
+
+  if (searchInput) {
+    getCitySearch(searchInput);
+    searchInputEl.value = '';
+  }
+  else {
+    alert('Search format should be city, state code, country code');
+  }
+};
+var buttonClickHandler = function (e) {
+  var buttonClickTxt = (e.target.innerHTML);
+  if (buttonClickTxt) {
+    getCitySearch(buttonClickTxt);
+    searchInputEl.value = '';
+  }
+};
+
+var newButton = function (city) {
+  var newButtonEl = document.createElement('button');
+  newButtonEl.setAttribute('class', 'list-group-item list-group-item-action');
+  newButtonEl.classList.add('buttons');
+  newButtonEl.setAttribute('type', 'button');
+  newButtonEl.setAttribute('id', city);
+  newButtonEl.addEventListener("click", buttonClickHandler);
+  var buttonTxt = document.createTextNode(city);
+  newButtonEl.appendChild(buttonTxt);
+  buttonDiv.prepend(newButtonEl);
+  buttonArray.push(city);
+  console.log(city)
+  console.log(buttonArray)
+  localStorage.setItem('savedSearch', JSON.stringify(buttonArray));
+  searchInputEl.value = '';
+}
+
+// THEN I am presented with current and future conditions for that city and that city is added to the search history 
+// use the city search to apply the lat/lon element to the call
+// calls geocoding API to use lat and lon in next function
+// set up form element text guide that says enter city, state code, and country code
+
+
+function getCitySearch(searchString) {
+  // ternary operator, if the value being passed to the function is a string, or a click event
+  var searchInput = typeof searchString === "string" ? searchString : searchInputEl.value.trim();
+
+  console.log(searchInput);
   var apiURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + searchInput + '&limit=10&appid=3b41e908f8123a87745091fffda5bb2b';
   fetch(apiURL)
     .then(function (response) {
@@ -65,20 +99,9 @@ function getCitySearch () {
       var country = data[0].country;
       var nameData = currentName + ', ' + currentState + ', ' + country;
       cityName.textContent = nameData;
-      // when dealing with multiple search results, append boostrap 'select' list to search field
-      // for (var i = 0; i < data.length; i++) {
-      // console.log(data[i].lat);
-      // console.log(data[i].lon);
-      // }
-      var buttonEl = document.createElement('button');
-      buttonEl.setAttribute('class', 'list-group-item list-group-item-action');
-      buttonEl.classList.add('buttons');
-      buttonEl.setAttribute('type', 'button');
-      var buttonTxt = document.createTextNode(nameData);
-      // var textGrab = buttonDiv.children.innerHTML
-      buttonEl.appendChild(buttonTxt);
-      buttonDiv.prepend(buttonEl);
-      buttonArray.push(buttonTxt);
+      // loop over buttonEl.innerhtml  
+      // if nameData !=== buttonEl.innerHTML[i]
+      newButton(nameData);
       var lat = data[0].lat;
       var lon = data[0].lon;
       var weathURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=3b41e908f8123a87745091fffda5bb2b';
@@ -100,14 +123,11 @@ function getCitySearch () {
           document.getElementById('current-display').removeAttribute('hidden');
           document.getElementById('forecast-table').removeAttribute('hidden');
 
-          console.log(data);
 
           /*WHEN I view current weather conditions for that city
           THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index*/
           var timeStamp = data.current.dt;
-          console.log(data.current.dt);
           var today = moment.unix(timeStamp).format("dddd, MMMM Do YYYY");
-          console.log(today);
           var iconCode = data.current.weather[0].icon;
           date.textContent = ' - ' + today;
           icon.src = "http://openweathermap.org/img/wn/" + iconCode + ".png";
@@ -118,24 +138,23 @@ function getCitySearch () {
           // WHEN I view the UV index, THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe.
           // apply class with conditionals like we did in the last assignment
           var currentUVI = data.current.uvi;
-          console.log(typeof currentUVI);
           if (currentUVI <= 2) {
-            uvIndex.classList.add('UV-lo')
+            uvIndex.classList.add('UV-lo');
           } else if (currentUVI <= 4) {
-            uvIndex.classList.add('UV-med-lo')
+            uvIndex.classList.add('UV-med-lo');
           } else if (currentUVI <= 6) {
-            uvIndex.classList.add('UV-med')
+            uvIndex.classList.add('UV-med');
           } else if (currentUVI <= 8) {
-            uvIndex.classList.add('UV-med-hi')
+            uvIndex.classList.add('UV-med-hi');
           } else {
-            uvIndex.classList.add('UV-hi')
+            uvIndex.classList.add('UV-hi');
           };
 
           // WHEN I view future weather conditions for that city THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
           // looping through table elements with 5-day forecast data.
           // loop for date in <thead>
           var dailyForecastDate = document.querySelectorAll('.forecast-date');
-          console.log(dailyForecastDate);
+
           for (let i = 0; i < dailyForecastDate.length; i++) {
             const dailyDateEl = dailyForecastDate[i];
             const tHeadDate = data.daily[i].dt;
@@ -145,7 +164,7 @@ function getCitySearch () {
 
           // loop for icon
           var dailyForecastIcon = document.querySelectorAll('.forecast-icon');
-          console.log(dailyForecastIcon);
+
           for (let i = 0; i < dailyForecastIcon.length; i++) {
             const dailyIconEl = dailyForecastIcon[i];
             const dailyIconCode = data.daily[i].weather[0].icon;
@@ -154,37 +173,27 @@ function getCitySearch () {
 
           // loop for low temp
           var lowTempEls = document.querySelectorAll('.lo-temp');
-          console.log(lowTempEls);
+
           for (let i = 0; i < lowTempEls.length; i++) {
             const lowTempTxt = lowTempEls[i];
             lowTempTxt.innerText = 'low: ' + Math.round(data.daily[i].temp.min) + ' °F';
           }
           // loop for high temp
           var highTempEls = document.querySelectorAll('.hi-temp');
-          console.log(highTempEls);
           for (let i = 0; i < highTempEls.length; i++) {
             const highTempTxt = highTempEls[i];
             highTempTxt.innerText = 'high: ' + Math.round(data.daily[i].temp.max) + '°F';
           }
           // loop for humidity 
           var dailyHumidityEls = document.querySelectorAll('.humidity');
-          console.log(dailyHumidityEls);
           for (let i = 0; i < dailyHumidityEls.length; i++) {
             const humidityTxt = dailyHumidityEls[i];
             humidityTxt.innerText = 'humidity: ' + data.daily[i].humidity + '%';
           }
-          buttonArray.push(buttonEl.innerHTML);
-          localStorage.setItem('savedSearch', JSON.stringify(buttonArray));
         });
     });
-  searchInputEl.textContent= '';
-};
-// event delegation for event listener
+}
+searchButton.addEventListener('click', getCitySearch);
 
-searchButton.addEventListener('click', function (e) {
-  e.preventDefault();
-  getCitySearch();
- });
- getSaves();
 
 
